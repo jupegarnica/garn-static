850e8c175d78abc1f4065540c446b733e9b546ed
jupegarnica <jupegarnica@gmail.com>
2021-10-30 11:49:46 +0000

    format: apply auto format

--- test.ts
+++ test.ts
@@ -1,15 +1,16 @@
 import { assert } from "https://deno.land/std@0.84.0/_util/assert.ts";
 import { serve } from "./main.ts";
-import {assertEquals} from 'https://deno.land/std/testing/asserts.ts'
+import { assertEquals } from "https://deno.land/std/testing/asserts.ts";
 
 Deno.test("must work", async () => {
-    const server = serve();
-    server.run();
-    const body = await fetch('http://localhost:8080/test-data/hello').then(r => r.text())
-    assertEquals(body, 'hola mundo')
-    server.abort();
+  const server = serve();
+  server.run();
+  const body = await fetch("http://localhost:8080/test-data/hello").then((r) =>
+    r.text()
+  );
+  assertEquals(body, "hola mundo");
+  server.abort();
 });
-
 
 // Deno.test("cli must work", async () => {
 //    Deno.run('deno run --allow-net --allow-read main.ts test-data 8081'.split(''))
