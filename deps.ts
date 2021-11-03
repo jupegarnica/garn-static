50ad29afc70f8ceb74ffef1fd46b83291b0ca56d
Jupegarnica <juan@geekshubs.com>
2021-11-03 10:16:02 +0100

    chore: deps script fixed

--- deps.ts
+++ deps.ts
@@ -1,4 +1,4 @@
-export { contentType } from "https://deno.land/x/media_types@v2.7.1/mod.ts";
+// export { contentType } from "https://deno.land/x/media_types@v2.10.2/mod.ts";
 
 export { listenAndServe } from "https://deno.land/std@0.113.0/http/server.ts";
 export { join, parse } from "https://deno.land/std@0.113.0/path/mod.ts";
